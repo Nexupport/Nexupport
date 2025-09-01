3f52b48bbc0d958051c0a38c7949ec073532f24d
Niels Krijnen <niels.krijnen@icloud.com>
2025-09-01 16:57:51 +0200

    Update auth test and rename workflow for clarity

--- supabase/tests/auth_test.sql
+++ supabase/tests/auth_test.sql
@@ -1,7 +1,7 @@
 BEGIN;
 SELECT plan(1);
 
-SELECT has_column('auth', 'users', 'uuid', 'uuid should exist');
+SELECT has_column('auth', 'users', 'id', 'id should exist');
 
 SELECT * FROM finish();
 ROLLBACK;
