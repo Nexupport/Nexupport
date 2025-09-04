800b7a572870ebdbbbb1a656aa7c936fdf994790
Niels Krijnen <niels.krijnen@icloud.com>
2025-09-04 15:46:33 +0200

    Enhance Supabase integration: scope type generation to public schema, add client initialization in layouts, and handle auth state changes.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
   import '../app.css';
+  import { onMount } from "svelte";
+  import { invalidate } from "$app/navigation";
 
-  let { children } = $props();
+  let { data, children } = $props();
+  const { supabase, session } = $derived(data)
+
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((_, _session) => {
+      if (_session?.expires_at !== session?.expires_at) {
+        invalidate("supabase:auth")
+      }
+    })
+
+    return () => data.subscription.unsubscribe()
+  })
 </script>
 
 {@render children()}
---
+++ src/routes/+layout.ts
@@ -0,0 +1,25 @@
+import type { LayoutLoad } from "./$types";
+import { createBrowserClient, createServerClient, isBrowser } from "@supabase/ssr";
+import { env } from "$env/dynamic/public"
+
+export const load: LayoutLoad = async ({ fetch, data, depends }) => {
+  depends("supabase:auth")
+
+  const supabase = isBrowser()
+    ? createBrowserClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY, {
+      global: { fetch }
+    })
+    : createServerClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY, {
+      global: { fetch },
+      cookies: {
+        getAll() {
+          return data.cookies
+        }
+      }
+    })
+  ;
+
+  const { data: { session } } = await supabase.auth.getSession()
+
+  return { supabase, session }
+}
